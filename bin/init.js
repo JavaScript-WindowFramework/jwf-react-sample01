607b4adaa5169f6ac137d9a55a89f14d41bf051a
SoraKumo <info@croud.jp>
2019-09-28 22:30:30 +0900

    no message

--- bin/init.js
+++ bin/init.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-if(fs.existsSync('../../../bin/init.js'))
+if(fs.existsSync('../../../bin/init.js') || fs.existsSync('bin/init.js'))
   process.exit(-1);
 console.log('copy template')
 fs.copySync(path.resolve(__dirname, '..'), '../../../',{overwrite:true});
